acb32b4fad1f2d72baa24587a60f2e944e13d7f6
VieiraLaris <lalasarandi@gmail.com>
2025-04-23 15:43:34 -0300

    adjusting infobox

--- src/components/InfoBox/InfoBox.tsx
+++ src/components/InfoBox/InfoBox.tsx
@@ -7,17 +7,22 @@ interface StyledInfoBoxProps {
 
 const StyledInfoBox: React.FC<StyledInfoBoxProps> = ({children}) => {
     const StyledInfoBox = styled("button")(({ theme }) => ({
-        backgroundColor: theme.palette.secondary.light,
+        backgroundColor: "#F4C3BE",
         border: `1px solid ${theme.palette.primary.main}`,
         borderRadius: "3px",
-        padding: "15px",
-        width: "100%",
+        padding: "5px 15px",        
         color: theme.palette.primary.main,
         display: "flex", 
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
         gap: "10px",
+        [theme.breakpoints.up('xs')]: {
+            width: "100%",
+        },
+        [theme.breakpoints.up('md')]: {
+            width: "250px",
+        },
       }));
 
     return (
