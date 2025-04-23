50c608fd51e22098cceebea1b6b50ab3865ce458
VieiraLaris <lalasarandi@gmail.com>
2025-04-23 10:26:00 -0300

    maximizing size of the navbar button's clicakble area

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,28 +1,39 @@
 import { AppBar, MenuItem, styled, Toolbar, Typography } from "@mui/material"
+import theme from "../../theme"
 
 const NavBar = () => {
-    const StyledToolbar = styled(Toolbar)(({theme})=> ({
-       display: "flex",
-       justifyContent: "space-evenly",
-       color: theme.palette.secondary.light
-    }))
+    const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+      display: "flex",
+      justifyContent: "space-evenly",
+      width: "100%",
+      alignItems: "center",
+      color: theme.palette.secondary.light,
 
+    }))
+  
+    const StyledMenuItem = styled(MenuItem)({
+      padding: "16px 24px", 
+      width: "100%",
+      justifyContent: "center",
+      '&:hover': {
+            backgroundColor: theme.palette.primary.light
+      }    
+    })
+  
     return (
-      <>
-        <AppBar position="absolute">
-            <StyledToolbar>
-                <MenuItem>
-                    <Typography variant="h6">About</Typography>
-                </MenuItem>
-                <MenuItem>
-                    <Typography variant="h6">Skills</Typography>
-                </MenuItem>
-                <MenuItem>
-                    <Typography variant="h6">Projects</Typography>
-                </MenuItem>
-            </StyledToolbar>
-        </AppBar>
-      </>
+      <AppBar position="absolute">
+        <StyledToolbar>
+          <StyledMenuItem>
+            <Typography variant="h6">About</Typography>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <Typography variant="h6">Skills</Typography>
+          </StyledMenuItem>
+          <StyledMenuItem>
+            <Typography variant="h6">Projects</Typography>
+          </StyledMenuItem>
+        </StyledToolbar>
+      </AppBar>
     )
   }
   
