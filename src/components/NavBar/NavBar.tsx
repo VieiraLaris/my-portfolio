95d63e440a526f4e4411aa5e5ac1da74cfc54d44
VieiraLaris <lalasarandi@gmail.com>
2025-04-23 15:44:04 -0300

    adding  paths to navbar buttons

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,39 +2,46 @@ import { AppBar, MenuItem, styled, Toolbar, Typography } from "@mui/material"
 import theme from "../../theme"
 
 const NavBar = () => {
-    const StyledToolbar = styled(Toolbar)(({ theme }) => ({
-      display: "flex",
-      justifyContent: "space-evenly",
-      width: "100%",
-      alignItems: "center",
-      color: theme.palette.secondary.light,
+  const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-evenly",
+    width: "100%",
+    alignItems: "center",
+    color: theme.palette.secondary.light,
+  }))
 
-    }))
-  
-    const StyledMenuItem = styled(MenuItem)({
-      padding: "16px 24px", 
-      width: "100%",
-      justifyContent: "center",
-      '&:hover': {
-            backgroundColor: theme.palette.primary.light
-      }    
-    })
-  
-    return (
-      <AppBar position="absolute">
-        <StyledToolbar>
-          <StyledMenuItem>
-            <Typography variant="h6">About</Typography>
-          </StyledMenuItem>
-          <StyledMenuItem>
-            <Typography variant="h6">Skills</Typography>
-          </StyledMenuItem>
-          <StyledMenuItem>
-            <Typography variant="h6">Projects</Typography>
-          </StyledMenuItem>
-        </StyledToolbar>
-      </AppBar>
-    )
+  const StyledMenuItem = styled(MenuItem)({
+    padding: "16px 24px",
+    width: "100%",
+    justifyContent: "center",
+    '&:hover': {
+      backgroundColor: theme.palette.primary.light
+    }
+  })
+
+  // Função utilitária para scroll suave
+  const scrollToSection = (id: string) => {
+    const section = document.getElementById(id)
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" })
+    }
   }
-  
-  export default NavBar
+
+  return (
+    <AppBar position="absolute">
+      <StyledToolbar>
+        <StyledMenuItem onClick={() => scrollToSection("about")}>
+          <Typography variant="h6">About</Typography>
+        </StyledMenuItem>
+        <StyledMenuItem onClick={() => scrollToSection("skills")}>
+          <Typography variant="h6">Skills</Typography>
+        </StyledMenuItem>
+        <StyledMenuItem onClick={() => scrollToSection("projects")}>
+          <Typography variant="h6">Projects</Typography>
+        </StyledMenuItem>
+      </StyledToolbar>
+    </AppBar>
+  )
+}
+
+export default NavBar
